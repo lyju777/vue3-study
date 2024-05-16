0b7f94924b8ca36aace059cb6fdb91f01a5d30cf
lyju777 <lyju777@hanmail.net>
2024-05-16 11:32:17 +0900

    템플릿 문법

---
+++ src/lectures/6/App_TemplateSyntax.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>보간법</h2>
+    <p>{{ message }}</p>
+    <p v-once>{{ message }}</p>
+    <button @click="message = message + '!'">click</button>
+    <hr />
+    <h2>HTML</h2>
+    <p v-html="rawHtml"></p>
+    <hr />
+    <h2>속성 바인딩</h2>
+    <div :title="dynmicTitle">마우스를 올려보세요</div>
+    <input type="text" value="홍길동" :disabled="isInputDisabled" />
+    <input :="attrs" />
+    <h2>javaScript</h2>
+    {{ message.split('').reverse().join('') }}
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const message = ref('안녕하세요!')
+    const rawHtml = ref('<strong>안녕하세요</string>')
+
+    const dynmicTitle = ref('안녕하세요~~')
+    const isInputDisabled = ref(false)
+
+    const attrs = ref({
+      type: 'text',
+      value: '12345678',
+      disabled: false
+    })
+
+    return {
+      message,
+      rawHtml,
+      dynmicTitle,
+      isInputDisabled,
+      attrs
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './lectures/4/App.vue'
+import App from './App.vue'
 
 createApp(App).mount('#app')
