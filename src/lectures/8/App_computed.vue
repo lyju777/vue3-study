403be71fe52215afecc461c16910c02b7bcd4bf7
lyju777 <lyju777@hanmail.net>
2024-05-20 11:37:35 +0900

    Computed & class style바인딩

---
+++ src/lectures/8/App_computed.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h2>{{ teacher.name }}</h2>
+    <h3>강의가 있습니까?</h3>
+    <p>{{ hasLecture }}</p>
+    <p>{{ hasLecture }}</p>
+    <p>{{ existLecture() }}</p>
+    <p>{{ existLecture() }}</p>
+    <button @click="counter++">counter:{{ counter }}</button>
+    <h2>
+      이름
+      <p>{{ fullName }}</p>
+    </h2>
+  </div>
+</template>
+
+<script>
+import { computed, reactive, ref } from 'vue'
+
+export default {
+  setup() {
+    const teacher = reactive({
+      name: '짐코딩',
+      lectures: ['HTML/CSS', 'Javascript', 'Vue3']
+    })
+
+    const hasLecture = computed(() => {
+      console.log('computed')
+      return teacher.lectures.length > 0 ? ' 있음' : '없음'
+    })
+
+    const existLecture = () => {
+      console.log('method')
+      return teacher.lectures.length > 0 ? ' 있음' : '없음'
+    }
+
+    const counter = ref(0)
+
+    const firstName = ref('홍')
+    const lastName = ref('길동')
+    const fullName = computed({
+      get() {
+        return firstName.value + ' ' + lastName.value
+      },
+      set(value) {
+        ;[firstName.value, lastName.value] = value.split(' ')
+      }
+    })
+    console.log('콘솔출력:', fullName.value)
+    fullName.value = '짐 코딩'
+    return {
+      teacher,
+      hasLecture,
+      existLecture,
+      counter,
+      fullName
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/lectures/9/App _class_binding.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="text" :class="[activeClass, errorClass]">텍스트 입니다.</div>
+    <button @click="toggle">toggle</button>
+    <button @click="hasError = !hasError">toggleError</button>
+  </div>
+</template>
+
+<script>
+import { computed, reactive, ref } from 'vue'
+
+export default {
+  setup() {
+    const isActive = ref(true)
+    const hasError = ref(false)
+
+    // const classObject = reactive({
+    //   active: true,
+    //   'text-danger': true
+    // })
+    const classObject = computed(() => {
+      return {
+        active: true,
+        'text-danger': true
+      }
+    })
+
+    const activeClass = ref('active')
+    const errorClass = ref('error')
+
+    const toggle = () => {
+      isActive.value = !isActive.value
+    }
+    return { isActive, toggle, hasError, classObject, activeClass, errorClass }
+  }
+}
+</script>
+
+<style scoped>
+.active {
+  font-weight: 900;
+}
+
+.text-danger {
+  color: red;
+}
+</style>
